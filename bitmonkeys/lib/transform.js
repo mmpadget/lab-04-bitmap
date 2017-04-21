516ee976a248d83b470ed2d468ebaebb58b09f41
Michael Padget <mpadget@comcast.net>
2017-04-21 16:18:34 -0700

    Encoding example

--- bitmonkeys/lib/transform.js
+++ bitmonkeys/lib/transform.js
@@ -18,6 +18,18 @@ console.log(bmp.colorArray);
 // console.log(bmp.colorArray.toString('hex'));
 bmp.Bitmask = bitmap.readUInt32LE(10);
 
+// // 3. The readfile.js runs.
+// const readMainFile = require('./lib/readfile.js');
+// // Runs the code in readfile.js
+// console.log(readMainFile, 'here');
+// // 1. readMainFile outputs an object.
+//
+// // 2. write and read to buffer.
+// const Buffer = require('buffer').Buffer;
+// let myBuffer = Buffer.from('Hello world');
+// console.log(myBuffer.toString('utf-8', 0, 5));
+// // (encoding, start, end)
+
 /*
 Type	Begin Byte	End Byte	Read	Output	Note
 spec	0	2	readUInt16LE(0)  ??	BM
