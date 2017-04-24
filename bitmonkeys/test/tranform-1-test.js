ec22cb2805e613c8101e8bf727e5dc1b924f0835
Chris Closser <chrisclosser1@gmail.com>
2017-04-24 10:49:09 -0700

    test for three files being made

---
+++ bitmonkeys/test/tranform-1-test.js
@@ -0,0 +1,29 @@
+const expect = require('chai').expect;
+const file = require('chai-files').file;
+const run = require('../lib/transform.js');
+const read = require('../lib/read.js');
+
+// test that we arre creating a new bitmap
+describe('write new grayscale file', function(){
+  it('should write a new gray scale copy of the bitmap', done =>{
+    read.readBit('bitmap.bmp', 'newBMPgray.bmp', run.grayscale);
+    expect(file('newBMPgray.bmp')).to.exist;
+    done();
+  });
+});
+
+describe('write new inverted file', function(){
+  it('should write a new inverted copy of the bitmap', done =>{
+    read.readBit('bitmap.bmp', 'newBMPinvert.bmp', run.grayscale);
+    expect(file('newBMPinvert.bmp')).to.exist;
+    done();
+  });
+});
+
+describe('write new scaled color file', function(){
+  it('should write a new scaled color copy of the bitmap', done =>{
+    read.readBit('bitmap.bmp', 'newBMPscale.bmp', run.grayscale);
+    expect(file('newBMPscale.bmp')).to.exist;
+    done();
+  });
+});
