7eea67ab63075449c502ab293a96486832ca69cc
Steven Johnson <stevenjohnson86@gmail.com>
2017-04-24 11:41:22 -0700

    cleaned code and fixed conflict

--- bitmonkeys/index.js
+++ bitmonkeys/index.js
@@ -3,8 +3,8 @@
 const read = require('./lib/read.js');
 const xform = require('./lib/transform.js');
 
-// read.readBit('bitmap.bmp', 'newBMPgray.bmp', xform.grayscale);
-// read.readBit('bitmap.bmp', 'newBMPinvert.bmp', xform.invert);
-// read.readBit('bitmap.bmp', 'newBMPRed.bmp', xform.scaleRed);
-// read.readBit('bitmap.bmp', 'newBMPGreen.bmp', xform.scaleGreen);
-// read.readBit('bitmap.bmp', 'newBMPBlue.bmp', xform.scaleBlue);
+read.readBit('bitmap.bmp', 'newBMPgray.bmp', xform.grayscale);
+read.readBit('bitmap.bmp', 'newBMPinvert.bmp', xform.invert);
+read.readBit('bitmap.bmp', 'newBMPRed.bmp', xform.scaleRed);
+read.readBit('bitmap.bmp', 'newBMPGreen.bmp', xform.scaleGreen);
+read.readBit('bitmap.bmp', 'newBMPBlue.bmp', xform.scaleBlue);
