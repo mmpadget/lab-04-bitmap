67122301a7836a9a71c5df557a8eceb62d6a8549
Steven Johnson <stevenjohnson86@gmail.com>
2017-04-24 09:26:29 -0700

    expanded scaleColor method to three methods, one for each color

--- bitmonkeys/index.js
+++ bitmonkeys/index.js
@@ -11,4 +11,4 @@ const xform = require('./lib/transform.js');
 // }
 read.readBit('bitmap.bmp', 'newBMPgray.bmp', xform.grayscale);
 read.readBit('bitmap.bmp', 'newBMPinvert.bmp', xform.invert);
-read.readBit('bitmap.bmp', 'newBMPscale.bmp', xform.scaleColor);
+read.readBit('bitmap.bmp', 'newBMPscale.bmp', xform.scaleRed);
